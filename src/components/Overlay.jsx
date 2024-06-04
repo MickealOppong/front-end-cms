e6e026a349927c9aa26614a84ca6149d8d0ca688
yaw oppong <opp.mic@gmail.com>
2024-06-04 21:30:19 +0200

    third

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,5 +1,5 @@
 const Overlay = () => {
-  return <div className="absolute top-0 bottom-0 left-0 right-0 z-[90] bg-slate-200">
+  return <div className="absolute top-0 bottom-0 left-0 right-0 z-[90] ">
 
   </div>
 }
