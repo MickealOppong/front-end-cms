29298e1d5e41fb61cda6a980229df69817f91a5b
yaw oppong <opp.mic@gmail.com>
2024-07-12 13:23:19 +0200

    css

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -49,7 +49,7 @@ const Products = () => {
 
   return <section className={`mt-36 lg:mt-8 h-[300vh] px-8 max-w-9xl mx-auto`
   }>
-    <div className={`flex flex-col gap-y-4 md:flex-row bg-white lg:gap-x-2 md:justify-between p-4 border-2 rounded `}>
+    <div className={`flex flex-col gap-y-4 md:flex-row bg-white lg:gap-x-2 md:justify-between p-4 border-2 rounded w-11/12`}>
       <Form className="flex flex-col md:flex-row items-center  gap-x-4 lg:px-4">
         {/**PAGE SIZE  */}
         <div className="hidden gap-x-2 items-center">
