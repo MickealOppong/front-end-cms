a40dfcaefce58539178793ae47789f4ce110069b
yaw oppong <opp.mic@gmail.com>
2024-07-12 12:40:02 +0200

    css

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -14,7 +14,7 @@ const HomeLayout = () => {
   const navigation = useNavigation();
   const isLoading = navigation.state === 'submitting'
 
-  return <main className="flex w-full">
+  return <main className="flex">
     <div className={`${showSidebar ? 'fixed top-0' : 'hidden'} duration-300 h-full  overflow-y-scroll no-scrollbar`}>
       <SidebarContainer />
     </div>
@@ -22,7 +22,7 @@ const HomeLayout = () => {
       <SmallSidebarContainer />
     </div>
     <div className={`flex flex-col lg:fixed 
-     min-h-[100vh] lg:top-0 ${showSidebar ? 'lg:left-[20rem] lg:w-[80rem]' : 'lg:left-0 lg:w-full'} duration-300 lg:right-0 border-r-2 w-full`} >
+     min-h-[100vh] lg:top-0 ${showSidebar ? 'lg:left-80 lg:w-[80vw]' : 'lg:left-0 lg:w-full'} duration-300 lg:right-0 border-r-2 w-full`} >
       <Navbar />
       <div className={`flex bg-slate-100 sticky top-20 h-[100vh] overflow-y-scroll no-scrollbar`}>{
         isLoading ?
