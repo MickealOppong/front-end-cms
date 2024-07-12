14691b4dc8c1df21e1c87b74bfa98a5f818a6fbd
yaw oppong <opp.mic@gmail.com>
2024-07-12 13:48:20 +0200

    css

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -47,7 +47,7 @@ const Products = () => {
   }
 
 
-  return <section className={`mt-36 lg:mt-8 h-[300vh] px-8 w-11/12`
+  return <section className={`mt-36 lg:mt-8 h-[300vh] px-8 w-11/12 ${showSidebar ? '' : '-ml-10'}`
   }>
     <div className={`flex flex-col gap-y-4 md:flex-row bg-white lg:gap-x-2 md:justify-between p-4 border-2 rounded `}>
       <Form className="flex flex-col md:flex-row items-center  gap-x-4 lg:px-4 w-full">
