7876d582ebcb47cfd7a09493c6301b477b47a1e3
Michael Oppong <151684371+MickealOppong@users.noreply.github.com>
2024-07-14 08:34:47 +0200

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const customFetch = axios.create({
-  baseURL: 'https://cms-local-api.up.railway.app'
+  baseURL: 'https://cms-local.up.railway.app'
 })
 
 export const getFromLocalStorage = (item) => {
