aa39df729c50173792a68eab7e4f1eee3bf361aa
yaw oppong <opp.mic@gmail.com>
2024-07-14 13:32:02 +0200

    second

--- src/util/index.js
+++ src/util/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const customFetch = axios.create({
-  baseURL: 'https://cms-local.up.railway.app:8080'
+  baseURL: 'https://cms-local.up.railway.app'
 })
 
 export const getFromLocalStorage = (item) => {
